5025f14a61d54f9f940e9391cd8b8a5b50a54e78
nshankaz12 <nkazary@calstatela.edu>
2021-11-22 17:25:11 -0800

    Added Bootstrap for Login, Register, and About Us
    Added Bootstrap for Login, Register, and About Us

--- bookEx/bookMng/forms.py
+++ bookEx/bookMng/forms.py
@@ -4,6 +4,22 @@ from .models import Book
 from .models import BookRating
 from .models import Messages
 from .models import IndivMessages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
 class BookForm(ModelForm):
--- bookEx/bookMng/views.py
+++ bookEx/bookMng/views.py
@@ -13,13 +13,13 @@ from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 
 
 class Register(CreateView):
     template_name = 'registration/register.html'
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('register-success')
 
     def form_valid(self, form):
